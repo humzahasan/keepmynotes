5f7184d6b11c31f5cc0eea2031b8cedc4e18ce52
Humza Hasan <humzahasan2101@gmail.com>
2021-05-06 19:47:43 +0530

    Delete functionality added

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {projectFirestore, timestamp} from '../config/firebase';
+import {AiFillDelete} from 'react-icons/ai';
 const Sidebar = ({setNoteId}) => {
   const [currentTitle, setCurrentTitle] = useState('');
   const [notes, setNotes] = useState([]);
@@ -8,7 +9,7 @@ const Sidebar = ({setNoteId}) => {
   useEffect(() => {
     const unsub = projectFirestore
       .collection('notes')
-      .orderBy('createdAt', 'asc')
+      .orderBy('createdAt', 'desc')
       .onSnapshot((snap) => {
         let documents = [];
         snap.forEach((doc) => {
@@ -20,9 +21,15 @@ const Sidebar = ({setNoteId}) => {
   }, []);
 
   const submitHandler = (e) => {
+    console.log('SUBMIT');
     e.preventDefault();
     notesRef.add({title: currentTitle, body: '', createdAt: timestamp()});
     setCurrentTitle('');
+  };
+
+  const deleteNote = (id) => {
+    projectFirestore.collection('notes').doc(id).delete();
+    setNoteId('');
   };
   return (
     <div className='sidebar'>
@@ -41,20 +48,26 @@ const Sidebar = ({setNoteId}) => {
         </form>
       </div>
       <div className='sidebar__notes'>
-        {notes.map((note) => (
-          <div
-            className='notes__container'
-            key={note.id}
-            onClick={() => {
-              setNoteId(note.id);
-            }}
-          >
-            <p className='notes__title'>{note.title}</p>
-            {note.body ? (
-              <p className='notes__body'>{note.body}</p>
-            ) : (
-              <p className='notes__bodydefault'>Add note description</p>
-            )}
+        {notes?.map((note) => (
+          <div className='notes__container' key={note.id}>
+            <div
+              className='notes__containerInner'
+              onClick={() => {
+                setNoteId(note?.id);
+              }}
+            >
+              <p className='notes__title'>{note?.title}</p>
+              {note?.body ? (
+                <p className='notes__body'>{note.body}</p>
+              ) : (
+                <p className='notes__bodydefault'>Add note description</p>
+              )}
+            </div>
+            <div className='notes__button'>
+              <button type='button' onClick={() => deleteNote(note.id)}>
+                <AiFillDelete />
+              </button>
+            </div>
           </div>
         ))}
       </div>
