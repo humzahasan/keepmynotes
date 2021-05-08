df751efae21f732e521ca0eb76afc1ec6547d79d
Humza Hasan <humzahasan2101@gmail.com>
2021-05-08 16:05:53 +0530

    feat: Authentication added

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,29 +1,53 @@
-import React, {useEffect, useState} from 'react';
-import {projectFirestore, timestamp} from '../config/firebase';
+import React, {useContext, useEffect, useState} from 'react';
+import {
+  googleprovider,
+  projectAuth,
+  projectFirestore,
+  timestamp,
+} from '../config/firebase';
 import {AiFillDelete} from 'react-icons/ai';
+import { FcGoogle } from "react-icons/fc";
+import {FiLogOut} from 'react-icons/fi';
+import {userContext} from '../provider/UserProvider';
 const Sidebar = ({setNoteId}) => {
+  const user = useContext(userContext);
   const [currentTitle, setCurrentTitle] = useState('');
   const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
   const notesRef = projectFirestore.collection('notes');
 
+  //console.log(user?.email);
+
   useEffect(() => {
-    const unsub = projectFirestore
-      .collection('notes')
-      .orderBy('createdAt', 'desc')
-      .onSnapshot((snap) => {
-        let documents = [];
-        snap.forEach((doc) => {
-          documents.push({...doc.data(), id: doc.id});
+    const getNotes = () => {
+      projectFirestore
+        .collection('notes')
+        .where('createdBy', '==', user.email)
+        .onSnapshot((snap) => {
+          let documents = [];
+          snap.forEach((doc) => {
+            documents.push({...doc.data(), id: doc.id});
+          });
+          setNotes(documents);
         });
-        setNotes(documents);
-      });
-    return () => unsub;
-  }, []);
+    };
+
+    if (user) {
+      setLoading(false);
+      if (!loading) {
+        getNotes();
+      }
+    }
+  }, [loading, user]);
 
   const submitHandler = (e) => {
-    console.log('SUBMIT');
     e.preventDefault();
-    notesRef.add({title: currentTitle, body: '', createdAt: timestamp()});
+    notesRef.add({
+      title: currentTitle,
+      body: '',
+      createdBy: user.email,
+      createdAt: timestamp(),
+    });
     setCurrentTitle('');
   };
 
@@ -31,47 +55,76 @@ const Sidebar = ({setNoteId}) => {
     projectFirestore.collection('notes').doc(id).delete();
     setNoteId('');
   };
+
+  const handleLogin = () => {
+    projectAuth
+      .signInWithPopup(googleprovider)
+      .then((res) => console.log(res.user))
+      .catch((err) => console.log(err));
+  };
+
+  const handleSignOut = () => {
+    setNotes(null);
+    projectAuth.signOut();
+    setNoteId('');
+    setLoading(false);
+  };
+
   return (
-    <div className='sidebar'>
-      <div className='sidebar__add'>
-        <p>Add New Note</p>
-        <form onSubmit={submitHandler}>
-          <input
-            type='text'
-            placeholder='Note Title'
-            value={currentTitle}
-            onChange={(e) => setCurrentTitle(e.target.value)}
-          />
-          <button disabled={!currentTitle} type='submit'>
-            Create
-          </button>
-        </form>
-      </div>
-      <div className='sidebar__notes'>
-        {notes?.map((note) => (
-          <div className='notes__container' key={note.id}>
-            <div
-              className='notes__containerInner'
-              onClick={() => {
-                setNoteId(note?.id);
-              }}
-            >
-              <p className='notes__title'>{note?.title}</p>
-              {note?.body ? (
-                <p className='notes__body'>{note.body.slice(0, 60)}</p>
-              ) : (
-                <p className='notes__bodydefault'>Add note description</p>
-              )}
-            </div>
-            <div className='notes__button'>
-              <button type='button' onClick={() => deleteNote(note.id)}>
-                <AiFillDelete />
+    <>
+      {user ? (
+        <div className='sidebar'>
+          <div className='sidebar__add'>
+            <p>Add New Note</p>
+            <form onSubmit={submitHandler}>
+              <input
+                type='text'
+                placeholder='Note Title'
+                value={currentTitle}
+                onChange={(e) => setCurrentTitle(e.target.value)}
+              />
+              <button disabled={!currentTitle} type='submit'>
+                Create
               </button>
-            </div>
+            </form>
+            <button type='submit' className='btn-auth' onClick={handleSignOut}>
+              Sign Out&nbsp;<FiLogOut size='1.5em' />
+            </button>
           </div>
-        ))}
-      </div>
-    </div>
+          <div className='sidebar__notes'>
+            {notes?.map((note) => (
+              <div className='notes__container' key={note.id}>
+                <div
+                  className='notes__containerInner'
+                  onClick={() => {
+                    setNoteId(note?.id);
+                  }}
+                >
+                  <p className='notes__title'>{note?.title}</p>
+                  {note?.body ? (
+                    <p className='notes__body'>{note.body.slice(0, 60)}</p>
+                  ) : (
+                    <p className='notes__bodydefault'>Add note description</p>
+                  )}
+                </div>
+                <div className='notes__button'>
+                  <button type='button' onClick={() => deleteNote(note.id)}>
+                    <AiFillDelete />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className='sidebar__login'>
+          <h3>Please login to access your saved note</h3>
+          <button className='btn-auth' type='submit' onClick={handleLogin}>
+            Login with &nbsp;<FcGoogle size='2em' />
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
